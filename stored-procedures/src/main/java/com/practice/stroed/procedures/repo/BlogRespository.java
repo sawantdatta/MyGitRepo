4dfbe7e73ac65620cea39318104c04eee76d9a21
sawantdatta <sawant.datta@gmail.com>
2023-07-14 14:50:22 +0530

    space removed

--- stored-procedures/src/main/java/com/practice/stroed/procedures/repo/BlogRespository.java
+++ stored-procedures/src/main/java/com/practice/stroed/procedures/repo/BlogRespository.java
@@ -9,8 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.practice.stroed.procedures.entity.Blog;
 
 public interface BlogRespository extends JpaRepository<Blog, Integer> {
-
-	// custom query to search to blog post by title or content
 	List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
 
 	@Procedure(name = "Blog.getTitleById")
